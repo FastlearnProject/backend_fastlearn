9d924a57d0a1e0be20c83d5fba7e21d0049a04b5
Agustin26l <agustin.lopez2606@gmail.com>
2024-07-02 16:36:20 -0500

    fix: Solución de creación de tokens para el put, post, getONly de usuario

--- app/controllers/control.usuario.js
+++ app/controllers/control.usuario.js
@@ -1,125 +1,189 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import conexion from "../config/db.config.js";
-import { completo, incompleto } from "../message/mensajes.js";
-import { config } from "dotenv";
-config();
-
-// Creaciones usuario
 
 export const crearusuario = async (req, res) => {
-    const { nombre, correo, contrasenaPlain, fechaNacimiento, telefono } = req.body;
+  const { nombre, correo, contrasenaPlain, fechaNacimiento, telefono } =
+    req.body;
 
-    try {
-        const contrasenaHash = await bcrypt.hash(contrasenaPlain, 10);
-        const contrasena = contrasenaHash;
-        const respuesta = await conexion.query(`CALL sp_insertarusuario('${nombre}', '${correo}', '${contrasena}', '${fechaNacimiento}', 'null', 'null', '${telefono}')`);
+  try {
+    const contrasenaHash = await bcrypt.hash(contrasenaPlain, 10);
 
-        if (respuesta[0].affectedRows == 1) {
-            const idConsulta = await conexion.query("SELECT LAST_INSERT_ID() AS id");
-            const nuevoUsuarioId = idConsulta[0][0].id;
+    const respuesta = await conexion.query(
+      `CALL sp_insertarusuario(?, ?, ?, ?, ?, ?, ?)`,
+      [
+        nombre,
+        correo,
+        contrasenaHash,
+        fechaNacimiento,
+        "null",
+        "null",
+        telefono,
+      ]
+    );
 
-            const payload = {
-                id: nuevoUsuarioId,
-                nombre: nombre,
-                correo: correo
-            };
+    if (respuesta[0][0]) {
+      const usuario = respuesta[0][0];
+      const idUsuario = usuario[0].id_usuario;
+      const payload = {
+        id_usuario: idUsuario,
+        nombre: nombre,
+        correo: correo,
+      };
 
-            const token = jwt.sign(payload, process.env.TOKEN_PRIVATEKEY, { expiresIn: process.env.TOKEN_EXPIRES_IN });
+      const token = jwt.sign(payload, process.env.TOKEN_PRIVATEKEY, {
+        expiresIn: process.env.TOKEN_EXPIRES_IN,
+      });
 
-            return res.status(201).json({ message: "Usuario creado exitosamente", token }); // Asegúrate de enviar el token correctamente
-        } else {
-            return res.status(200).json({ message: "No se pudo crear el usuario" });
-        }
-    } catch (err) {
-        return res.status(500).json({ message: "Error en el servidor, por favor intentalo de nuevo más tarde" });
+      return res
+        .status(201)
+        .json({ message: "Usuario creado exitosamente", token });
+    } else {
+      return res.status(200).json({ message: "No se pudo crear el usuario" });
     }
+  } catch (err) {
+    console.error("Error al crear usuario:", err);
+    return res
+      .status(500)
+      .json({
+        message: "Error en el servidor, por favor intentalo de nuevo más tarde",
+      });
+  }
 };
-
-// Obtener usuario(s)
 
 export const mostrarusuario = async (req, res) => {
-    const id = req.params['id'];
-    try {
-        const respuesta = await conexion.query(`CALL sp_mostrarusuario(${id})`);
-        completo(req, res, 200, respuesta[0]);
-    } catch (err) {
-        incompleto(req, res, 400, respuesta[0]);
+  const id = req.params.id;
+  try {
+    const respuesta = await conexion.query(`CALL sp_mostrarusuario(${id})`);
+    if (respuesta[0].length > 0) {
+      return res.status(200).json(respuesta[0][0]);
+    } else {
+      return res.status(404).json({ message: "Usuario no encontrado" });
     }
-}
-
-export const mostrarusuarios = async (req, res) => {
-    try {
-        const respuesta = await conexion.query(`CALL sp_mostrarusuarios()`);
-        completo(req, res, 200, respuesta[0]);
-    } catch (err) {
-        incompleto(req, res, 400, respuesta[0]);
-    }
-}
-
-// Modificar usuario
-
-export const modificarusuario = async(req, res)=>{
-    const {id, nombre, correo, contrasenaPlain, fechaNacimiento, genero, telefono} = req.body;
-
-    try {
-        const contrasenaHash = await bcrypt.hash(contrasenaPlain, 10);
-        const contrasena = contrasenaHash;
-        const respuesta = await conexion.query(`CALL sp_modificarusuario(${id}, '${nombre}', '${correo}', '${contrasena}', '${fechaNacimiento}', '${genero}', '${telefono}')`);
-
-        if (respuesta[0].affectedRows == 1) {
-            completo(req, res, 201, "Usuario modificado exitosamente");
-        } else {
-            completo(req, res, 200, "No se pudo modificar el usuario");
-        }
-    } catch (err) {
-        incompleto(req, res, 500, "Error en el servidor, por favor intentalo de nuevo más tarde");
-    }
-}
-
-export const modificarRolUsuario = async (req, res) => {
-    const { rol } = req.body;
-    const { correo } = req.user;
-
-    try {
-        console.log("Correo del usuario:", correo);
-        console.log("Rol seleccionado:", rol);
-
-        const [result] = await conexion.query('CALL sp_modificarrol(?, ?)', [correo, rol]);
-
-        if (result.affectedRows === 1) {
-            const payload = { correo, rol };
-            const token = jwt.sign(payload, process.env.TOKEN_PRIVATEKEY, { expiresIn: process.env.TOKEN_EXPIRES_IN });
-
-            return res.status(200).json({ message: 'Rol actualizado exitosamente', token });
-        } else {
-            return res.status(404).json({ message: 'Usuario no encontrado' });
-        }
-    } catch (err) {
-        console.error('Error al actualizar el rol:', err);
-        return res.status(500).json({ message: 'Error en el servidor, por favor intentalo de nuevo más tarde' });
-    }
+  } catch (err) {
+    console.error("Error al mostrar usuario:", err);
+    return res
+      .status(500)
+      .json({
+        message: "Error en el servidor, por favor intentalo de nuevo más tarde",
+      });
+  }
 };
 
-export const eliminarusuario = async(req, res)=>{
-    const id = req.params['id'];
-    try {
-        const respuesta = await conexion.query(`CALL sp_eliminarusuario(${id})`);
-        if (respuesta[0].affectedRows == 1) {
-            completo(req, res, 200, "Usuario eliminado");
-        } else {
-            completo(req, res, 200, "Usuario no se pudo eliminar");
-        }
-    } catch (err) {
-        incompleto(req, res, 400, "Error interno: Intente de nuevo");
+export const mostrarusuarios = async (req, res) => {
+  try {
+    const respuesta = await conexion.query(`CALL sp_mostrarusuarios()`);
+    return res.status(200).json(respuesta[0]);
+  } catch (err) {
+    console.error("Error al mostrar usuarios:", err);
+    return res
+      .status(500)
+      .json({
+        message: "Error en el servidor, por favor intentalo de nuevo más tarde",
+      });
+  }
+};
+
+export const modificarusuario = async (req, res) => {
+  const {
+    id,
+    nombre,
+    correo,
+    contrasenaPlain,
+    fechaNacimiento,
+    genero,
+    telefono,
+  } = req.body;
+
+  try {
+    const contrasenaHash = await bcrypt.hash(contrasenaPlain, 10);
+    const contrasena = contrasenaHash;
+    const respuesta = await conexion.query(
+      `CALL sp_modificarusuario(${id}, '${nombre}', '${correo}', '${contrasena}', '${fechaNacimiento}', '${genero}', '${telefono}')`
+    );
+
+    if (respuesta[0].affectedRows === 1) {
+      return res
+        .status(200)
+        .json({ message: "Usuario modificado exitosamente" });
+    } else {
+      return res.status(404).json({ message: "Usuario no encontrado" });
     }
-}
+  } catch (err) {
+    console.error("Error al modificar usuario:", err);
+    return res
+      .status(500)
+      .json({
+        message: "Error en el servidor, por favor intentalo de nuevo más tarde",
+      });
+  }
+};
+
+export const modificarRolUsuario = async (req, res) => {
+  const { rol } = req.body;
+  const { id_usuario } = req.user;
+  const { correo } = req.user;
+
+  try {
+    console.log("Id del usuario:", id_usuario);
+    console.log("Correo del usuario:", correo);
+    console.log("Rol seleccionado:", rol);
+
+    const [result] = await conexion.query("CALL sp_modificarrol(?, ?)", [
+      correo,
+      rol,
+    ]);
+
+    console.log("Resultado de la consulta:", result);
+
+    if (result && result.affectedRows === 1) {
+      const payload = { id_usuario, correo, rol };
+      const token = jwt.sign(payload, process.env.TOKEN_PRIVATEKEY, {
+        expiresIn: process.env.TOKEN_EXPIRES_IN,
+      });
+
+      return res
+        .status(200)
+        .json({ message: "Rol actualizado exitosamente", token });
+    } else {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+  } catch (err) {
+    console.error("Error al actualizar el rol:", err);
+    return res
+      .status(500)
+      .json({
+        message: "Error en el servidor, por favor intentalo de nuevo más tarde",
+      });
+  }
+};
+
+export const eliminarusuario = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const respuesta = await conexion.query(`CALL sp_eliminarusuario(${id})`);
+    if (respuesta[0].affectedRows === 1) {
+      return res
+        .status(200)
+        .json({ message: "Usuario eliminado exitosamente" });
+    } else {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+  } catch (err) {
+    console.error("Error al eliminar usuario:", err);
+    return res
+      .status(500)
+      .json({
+        message: "Error en el servidor, por favor intentalo de nuevo más tarde",
+      });
+  }
+};
 
 export const logueoUsuario = async (req, res) => {
   const { correo, contrasena } = req.body;
 
   try {
-    const [rows] = await conexion.query('CALL sp_buscarusuario(?)', [correo]);
+    const [rows] = await conexion.query("CALL sp_buscarusuario(?)", [correo]);
 
     if (rows.length === 0 || rows[0].length === 0) {
       return res.status(401).json({ message: "Usuario no existe" });
@@ -133,15 +197,22 @@ export const logueoUsuario = async (req, res) => {
     }
 
     const payload = {
-      id: usuario.id,
+      id_usuario: usuario.id_usuario,
       correo: usuario.correo,
-      rol: usuario.rol // Asegúrate de que el rol esté incluido en el payload
+      rol: usuario.rol,
     };
 
-    const token = jwt.sign(payload, process.env.TOKEN_PRIVATEKEY, { expiresIn: process.env.TOKEN_EXPIRES_IN });
+    const token = jwt.sign(payload, process.env.TOKEN_PRIVATEKEY, {
+      expiresIn: process.env.TOKEN_EXPIRES_IN,
+    });
 
     return res.status(200).json({ message: "Inicio de sesión exitoso", token });
   } catch (err) {
-    return res.status(500).json({ message: "Error en el servidor, por favor intentalo de nuevo más tarde" });
+    console.error("Error al iniciar sesión:", err);
+    return res
+      .status(500)
+      .json({
+        message: "Error en el servidor, por favor intentalo de nuevo más tarde",
+      });
   }
 };
--- app/routes/routes.user.js
+++ app/routes/routes.user.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { crearusuario, mostrarusuarios, mostrarusuario, modificarusuario, eliminarusuario, logueoUsuario, modificarRolUsuario } from "../controllers/control.usuario.js";
+import { crearusuario, mostrarusuarios, mostrarusuario, modificarusuario, eliminarusuario, logueoUsuario, modificarRolUsuario, mostrarusuarioPorCorreo } from "../controllers/control.usuario.js";
 import { verifyToken } from "../middlewares/oauth.js";
 
 const rutaUser = Router();
@@ -8,7 +8,8 @@ rutaUser.post("/usuario", crearusuario);
 rutaUser.post("/login", logueoUsuario);
 
 rutaUser.get("/usuario", verifyToken, mostrarusuarios);
-rutaUser.get("/usuario/:id", verifyToken, mostrarusuario);
+rutaUser.get("/usuario/:id", mostrarusuario);
+
 rutaUser.put("/usuario/:id", verifyToken, modificarusuario);
 rutaUser.put("/rol", verifyToken, modificarRolUsuario);
 rutaUser.delete("/usuario/:id", verifyToken, eliminarusuario);
