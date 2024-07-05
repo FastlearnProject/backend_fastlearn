da7d63811bf9235164b0e648add533638653a42d
Agustin26l <agustin.lopez2606@gmail.com>
2024-07-05 14:26:36 -0500

    refactor: Creación de carpeta de distribución

--- dist/config/db.config.js
+++ dist/config/db.config.js
@@ -13,8 +13,8 @@ var conexion = (0, _promise.createPool)({
   password: process.env.MYSQLPASSWORD || "basedatos_fl",
   port: process.env.MYSQLPORT || 3306,
   database: process.env.MYSQLDATABASE || "basedatos_fl",
-  waitForConnections: process.env.WAIT || true,
-  connectionLimit: process.env.WAIT || 10,
-  queueLimit: process.env.QUEUE || 0
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 var _default = exports["default"] = conexion;
