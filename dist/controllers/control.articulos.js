7fc54e3b3d1c33df44b2609ed42fd15c04bafd8a
Agustin26l <agustin.lopez2606@gmail.com>
2024-07-17 11:12:47 -0500

    fix: Correción inserción de articulos

--- dist/controllers/control.articulos.js
+++ dist/controllers/control.articulos.js
@@ -6,46 +6,94 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.mostrarArticulos = exports.mostrarArticuloUsuario = exports.mostrarArticulo = exports.insertarArticulo = exports.eliminarArticulo = void 0;
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+var _slicedToArray2 = _interopRequireDefault(require("@babel/runtime/helpers/slicedToArray"));
 var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
-var _dbConfig = _interopRequireDefault(require("../config/db.config.js"));
+var _db = _interopRequireDefault(require("../config/db.config"));
 var insertarArticulo = exports.insertarArticulo = /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(req, res) {
-    var _req$body, id_usuario, titulo, texto1, texto2, texto3, texto4, respuesta;
+    var _req$body, id_usuario, titulo, texto1, texto2, texto3, texto4, query, _yield$conexion$execu, _yield$conexion$execu2, rows, _yield$conexion$execu3, _yield$conexion$execu4, _rows;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) switch (_context.prev = _context.next) {
         case 0:
-          _req$body = req.body, id_usuario = _req$body.id_usuario, titulo = _req$body.titulo, texto1 = _req$body.texto1, texto2 = _req$body.texto2, texto3 = _req$body.texto3, texto4 = _req$body.texto4;
-          _context.prev = 1;
-          _context.next = 4;
-          return _dbConfig["default"].query("CALL sp_insertararticulo(".concat(id_usuario, ", '").concat(titulo, "','").concat(texto1, "','").concat(texto2, "', '").concat(texto3, "', '").concat(texto4, "')"));
-        case 4:
-          respuesta = _context.sent;
-          if (!(respuesta[0].affectedRows == 1)) {
-            _context.next = 9;
+          _req$body = req.body, id_usuario = _req$body.id_usuario, titulo = _req$body.titulo, texto1 = _req$body.texto1, texto2 = _req$body.texto2, texto3 = _req$body.texto3, texto4 = _req$body.texto4; // Validar la entrada para prevenir inyección SQL
+          if (!(!id_usuario || !titulo || !texto1 || !texto2 || !texto3 || !texto4)) {
+            _context.next = 3;
+            break;
+          }
+          return _context.abrupt("return", res.status(400).json({
+            message: "Todos los campos son obligatorios"
+          }));
+        case 3:
+          // Uso de prepared statements para evitar inyección SQL
+          query = 'CALL sp_insertararticulo(?, ?, ?, ?, ?, ?)';
+          _context.prev = 4;
+          _context.next = 7;
+          return _db["default"].execute(query, [id_usuario, titulo, texto1, texto2, texto3, texto4]);
+        case 7:
+          _yield$conexion$execu = _context.sent;
+          _yield$conexion$execu2 = (0, _slicedToArray2["default"])(_yield$conexion$execu, 1);
+          rows = _yield$conexion$execu2[0];
+          if (!(rows.affectedRows === 1)) {
+            _context.next = 14;
             break;
           }
           return _context.abrupt("return", res.status(201).json({
             message: "Articulo creado exitosamente"
           }));
-        case 9:
+        case 14:
           return _context.abrupt("return", res.status(200).json({
             message: "No se pudo crear el articulo"
           }));
-        case 10:
-          _context.next = 16;
+        case 15:
+          _context.next = 39;
           break;
-        case 12:
-          _context.prev = 12;
-          _context.t0 = _context["catch"](1);
+        case 17:
+          _context.prev = 17;
+          _context.t0 = _context["catch"](4);
           console.error("Error al crear articulo:", _context.t0);
+
+          // Reintentar en caso de error de conexión
+          if (!(_context.t0.code === 'ECONNRESET')) {
+            _context.next = 38;
+            break;
+          }
+          _context.prev = 21;
+          _context.next = 24;
+          return _db["default"].execute(query, [id_usuario, titulo, texto1, texto2, texto3, texto4]);
+        case 24:
+          _yield$conexion$execu3 = _context.sent;
+          _yield$conexion$execu4 = (0, _slicedToArray2["default"])(_yield$conexion$execu3, 1);
+          _rows = _yield$conexion$execu4[0];
+          if (!(_rows.affectedRows === 1)) {
+            _context.next = 31;
+            break;
+          }
+          return _context.abrupt("return", res.status(201).json({
+            message: "Articulo creado exitosamente"
+          }));
+        case 31:
+          return _context.abrupt("return", res.status(200).json({
+            message: "No se pudo crear el articulo"
+          }));
+        case 32:
+          _context.next = 38;
+          break;
+        case 34:
+          _context.prev = 34;
+          _context.t1 = _context["catch"](21);
+          console.error("Error al reintentar crear articulo:", _context.t1);
           return _context.abrupt("return", res.status(500).json({
             message: "Error en el servidor, por favor inténtalo de nuevo más tarde"
           }));
-        case 16:
+        case 38:
+          return _context.abrupt("return", res.status(500).json({
+            message: "Error en el servidor, por favor inténtalo de nuevo más tarde"
+          }));
+        case 39:
         case "end":
           return _context.stop();
       }
-    }, _callee, null, [[1, 12]]);
+    }, _callee, null, [[4, 17], [21, 34]]);
   }));
   return function insertarArticulo(_x, _x2) {
     return _ref.apply(this, arguments);
@@ -59,7 +107,7 @@ var mostrarArticulos = exports.mostrarArticulos = /*#__PURE__*/function () {
         case 0:
           _context2.prev = 0;
           _context2.next = 3;
-          return _dbConfig["default"].query("CALL sp_mostrararticulos()");
+          return _db["default"].query("CALL sp_mostrararticulos()");
         case 3:
           respuesta = _context2.sent;
           return _context2.abrupt("return", res.status(200).json(respuesta[0]));
@@ -86,10 +134,10 @@ var mostrarArticulo = exports.mostrarArticulo = /*#__PURE__*/function () {
     return _regenerator["default"].wrap(function _callee3$(_context3) {
       while (1) switch (_context3.prev = _context3.next) {
         case 0:
-          id = req.params['id'];
+          id = req.params["id"];
           _context3.prev = 1;
           _context3.next = 4;
-          return _dbConfig["default"].query("CALL sp_mostrararticulo(".concat(id, ")"));
+          return _db["default"].query("CALL sp_mostrararticulo(".concat(id, ")"));
         case 4:
           respuesta = _context3.sent;
           return _context3.abrupt("return", res.status(200).json(respuesta[0]));
@@ -116,10 +164,10 @@ var mostrarArticuloUsuario = exports.mostrarArticuloUsuario = /*#__PURE__*/funct
     return _regenerator["default"].wrap(function _callee4$(_context4) {
       while (1) switch (_context4.prev = _context4.next) {
         case 0:
-          id = req.params['id'];
+          id = req.params["id"];
           _context4.prev = 1;
           _context4.next = 4;
-          return _dbConfig["default"].query("CALL sp_mostrararticulouser(".concat(id, ")"));
+          return _db["default"].query("CALL sp_mostrararticulouser(".concat(id, ")"));
         case 4:
           respuesta = _context4.sent;
           return _context4.abrupt("return", res.status(200).json(respuesta[0]));
@@ -149,7 +197,7 @@ var eliminarArticulo = exports.eliminarArticulo = /*#__PURE__*/function () {
           id = req.params.id;
           _context5.prev = 1;
           _context5.next = 4;
-          return _dbConfig["default"].query("CALL sp_eliminararticulo(".concat(id, ")"));
+          return _db["default"].query("CALL sp_eliminararticulo(".concat(id, ")"));
         case 4:
           respuesta = _context5.sent;
           if (!(respuesta[0].affectedRows === 1)) {
